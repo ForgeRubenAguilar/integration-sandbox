e844fd3358dfed4fc9bfeb0baa3d9932c6b38be0
Ruben Aguilar <ruben.aguilar@forgeglobal.com>
2021-04-02 22:12:57 -0700

    Scenario script files

---
+++ update-files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# updates both of the files created by new-files.sh
+
+echo "Wait, that's all garnish. The recipe calls for marinated lamb baked for 8 mins at 400F." >> src/PrepareTheSummon.sh
+
+git add --all
+git commit -m "Forgot the main dish."
+
+
+
+echo "w̵̢̤̰̲̤͆̐̄͂͗̅̀̍͌͊ͅȟ̷̨͇͇͙̝͆̄͛̇̚ë̴̡͔͈̹̜̫́͗̐̅͆́́͝͝ř̴̨̛̖̲͕̤͚̜̺̠͎͍̙̽̒̒̑͒̎̈́́͂ͅę̸̧̥̻̼̼̟̦͂̂̎̌́̃͋̈̕̕͝'̸̛̬̙͓͕͚͓̘̻͆́̉̾̎͛ş̵̪̠̄ ̵̧̫̳̦̗̝͎̮̫͉̓̿̆͒̃̂̒͌͝t̶̡̯̮̳̮̻̩̻̑̃̾̉͋̾̈́h̶̼̯̰̙͉̼͓̞͌̿̉̉͑̄ͅe̷̡̘̱̫̖̼̞̰͉̠͎̔͐͊̀̌̈̈́̓ ̷͙͔̩͍̼͔̾̓̇̀l̵̨͕̖͍̫̤͍̣̬̲̾̑̀͒̽̈́͒͠ą̷̜̎̂̔͌͒́̆̆͛̊̎́̕̚ͅm̴̨̡̢͔̻̮͉̺̫̖̭̎͑̋͐̽̑̌̾͋͆̓́̿̈̉͜b̶̧̨̢̘͖̗̠̦̖̺̬̓̅͐̄̓͌͗̊͊̄͜ ̴̧̯͐̄̀̏͑̓̈́̚͘͜s̵̱̬͕̿͒̃͆̄͊̃̊̈́̇̋́͘͝a̶̡̧͓͍̻̙̲̘͙͔͔̐͗̃̅͒ṷ̴́̾͌͑̓̏̒̽̈́̔̉c̵̢̝͕͔͎̘̤͖̭̖̹̯͇͖͐̈́͑͛̽̎̚͝é̶̛̦͉̣͈̘̀̓͆̈́͂̈́̆̈́̑͝͠?̶̞̥̮̰͓̉̀̅̒̈́͘!̸̡̧̪̮͉̜͖͔̰̭̘͖̌̄͗͜͜͜" >> src/SummonTheOne.sh
+
+
+git add --all
+git commit -m "Summoned wrong old god."
