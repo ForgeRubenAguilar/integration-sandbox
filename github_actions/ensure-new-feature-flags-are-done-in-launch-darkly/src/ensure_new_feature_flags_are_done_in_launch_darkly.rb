7370930ccddeccb1d14349fe745e0b7775a349a0
Ruben Aguilar <ruben.aguilar@forgeglobal.com>
2022-08-29 16:44:17 -0700

    Fail for slack integration

--- github_actions/ensure-new-feature-flags-are-done-in-launch-darkly/src/ensure_new_feature_flags_are_done_in_launch_darkly.rb
+++ github_actions/ensure-new-feature-flags-are-done-in-launch-darkly/src/ensure_new_feature_flags_are_done_in_launch_darkly.rb
@@ -13,6 +13,8 @@ require 'colorize'
 
 # --- main ---
 
+exit 1
+
 heroku_app_id = ENV.fetch("HEROKU_APP_ID")
 heroku_app_name = ENV.fetch("HEROKU_APP_NAME")
 heroku_release_id = ENV.fetch("HEROKU_RELEASE_ID")
